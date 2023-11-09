c88494b7bb3ce8cb94de596c14117be225e54513
HasChad <troll.yahya@gmail.com>
2023-11-09 23:18:50 +0300

    few updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use color_eyre::eyre::Result;
 use macroquad::prelude::*;
 
-const SCREEN_WIDTH: u16 = 1280;
-const SCREEN_HEIGHT: u16 = 720;
+const SCREEN_WIDTH: usize = 1280;
+const SCREEN_HEIGHT: usize = 720;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum CellState {
@@ -17,20 +17,18 @@ async fn main() -> Result<()> {
 
     request_new_screen_size(SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32);
 
-    let width: usize = (SCREEN_WIDTH) as usize;
-    let height: usize = (SCREEN_HEIGHT) as usize;
+    let width = SCREEN_WIDTH;
+    let height = SCREEN_HEIGHT;
 
     let mut buffer = vec![CellState::Dead; width * height];
 
     let mut _current_item = CellState::Sand;
 
-    //let mut xpos = vec![CellState::Dead; width];
-    //let mut ypos = vec![xpos; height];
-
-    let mut image = Image::gen_image_color(SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+    let mut image = Image::gen_image_color(SCREEN_WIDTH as u16, SCREEN_HEIGHT as u16, WHITE);
 
     let texture = Texture2D::from_image(&image);
 
+    //Simulation Loop
     loop {
         clear_background(BLACK);
 
@@ -60,9 +58,12 @@ async fn main() -> Result<()> {
         //Pixel iterate
         for y in (0..height).rev() {
             for x in 0..width {
+                //info!("x = {}, y = {}", x, y);
+
                 if x == width - 1 || x == 0 {
                     continue;
                 }
+
                 let pixel_pos: usize = (y * width) + x;
                 let left: usize = pixel_pos - 1;
                 let right: usize = pixel_pos + 1;
@@ -72,17 +73,17 @@ async fn main() -> Result<()> {
 
                 match buffer[pixel_pos] {
                     CellState::Sand => {
-                        if y != height - 1 {
+                        if down < width * height {
                             //Down
                             if buffer[down] == CellState::Dead {
                                 buffer[down] = CellState::Sand;
                                 buffer[pixel_pos] = CellState::Dead;
                             //Down left
-                            } else if buffer[down_left] == CellState::Dead {
+                            } else if buffer[down_left] == CellState::Dead && x > 0 {
                                 buffer[down_left] = CellState::Sand;
                                 buffer[pixel_pos] = CellState::Dead;
                             //Down right
-                            } else if buffer[down_right] == CellState::Dead {
+                            } else if buffer[down_right] == CellState::Dead && x < width - 1 {
                                 buffer[down_right] = CellState::Sand;
                                 buffer[pixel_pos] = CellState::Dead;
                                 //Water collision
@@ -94,22 +95,23 @@ async fn main() -> Result<()> {
                     }
 
                     CellState::Water => {
-                        let control = y != height - 2;
-
-                        //Down
-                        if buffer[down] == CellState::Dead && control {
-                            buffer[down] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-                        //Down left
-                        } else if buffer[down_left] == CellState::Dead && control {
-                            buffer[down_left] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-                        //Down right
-                        } else if buffer[down_right] == CellState::Dead && control {
-                            buffer[down_right] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
+                        if down < width * height {
+                            //Down
+                            if buffer[down] == CellState::Dead {
+                                buffer[down] = CellState::Water;
+                                buffer[pixel_pos] = CellState::Dead;
+                            //Down left
+                            } else if buffer[down_left] == CellState::Dead {
+                                buffer[down_left] = CellState::Water;
+                                buffer[pixel_pos] = CellState::Dead;
+                            //Down right
+                            } else if buffer[down_right] == CellState::Dead {
+                                buffer[down_right] = CellState::Water;
+                                buffer[pixel_pos] = CellState::Dead;
+                            }
+                        }
                         //Left
-                        } else if buffer[left] == CellState::Dead {
+                        else if buffer[left] == CellState::Dead {
                             buffer[left] = CellState::Water;
                             buffer[pixel_pos] = CellState::Dead;
                         //Right
