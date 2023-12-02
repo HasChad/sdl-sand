a1a1ed18e505d1c5fa7b689962fbfa50003fa8a6
HasChad <troll.yahya@gmail.com>
2023-12-02 04:10:04 +0300

    changed from macroquad to sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,45 @@
-use color_eyre::eyre::Result;
-use macroquad::prelude::*;
+extern crate sdl2;
 
-const SCREEN_WIDTH: usize = 1280;
-const SCREEN_HEIGHT: usize = 720;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-enum CellState {
-    Dead,
-    Sand,
-    Water,
-}
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
 
-#[macroquad::main("Sandbox")]
-async fn main() -> Result<()> {
-    color_eyre::install()?;
+    let window = video_subsystem
+        .window("rust-sdl2 demo: Video", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
 
-    request_new_screen_size(SCREEN_WIDTH as f32, SCREEN_HEIGHT as f32);
+    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    let width = SCREEN_WIDTH;
-    let height = SCREEN_HEIGHT;
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
 
-    let mut buffer = vec![CellState::Dead; width * height];
-
-    let mut _current_item = CellState::Sand;
-
-    let mut image = Image::gen_image_color(SCREEN_WIDTH as u16, SCREEN_HEIGHT as u16, WHITE);
-
-    let texture = Texture2D::from_image(&image);
-
-    //Simulation Loop
-    loop {
-        clear_background(BLACK);
-
-        //Mouse item changer
-        if is_key_pressed(KeyCode::Key2) {
-            _current_item = CellState::Water;
-        } else if is_key_pressed(KeyCode::Key1) {
-            _current_item = CellState::Sand;
-        }
-
-        //Mouse input
-        if is_mouse_button_down(MouseButton::Left) {
-            let (mut _mouse_posx, mut _mouse_posy) = mouse_position();
-            let mut _mousepos: usize = (_mouse_posy * width as f32 + _mouse_posx) as usize;
-
-            buffer[_mousepos - width] = _current_item;
-            buffer[_mousepos - width - 1] = _current_item;
-            buffer[_mousepos - width + 1] = _current_item;
-            buffer[_mousepos + width] = _current_item;
-            buffer[_mousepos + width - 1] = _current_item;
-            buffer[_mousepos + width + 1] = _current_item;
-            buffer[_mousepos] = _current_item;
-            buffer[_mousepos - 1] = _current_item;
-            buffer[_mousepos + 1] = _current_item;
-        }
-
-        //Pixel iterate
-        for y in (0..height).rev() {
-            for x in 0..width {
-                //info!("x = {}, y = {}", x, y);
-
-                if x == width - 1 || x == 0 {
-                    continue;
-                }
-
-                let pixel_pos: usize = (y * width) + x;
-                let left: usize = pixel_pos - 1;
-                let right: usize = pixel_pos + 1;
-                let down: usize = pixel_pos + width;
-                let down_left: usize = down - 1;
-                let down_right: usize = down + 1;
-
-                match buffer[pixel_pos] {
-                    CellState::Dead => continue,
-                    CellState::Sand => {
-                        if down < width * height {
-                            //Down
-                            if buffer[down] == CellState::Dead {
-                                buffer[down] = CellState::Sand;
-                                buffer[pixel_pos] = CellState::Dead;
-
-                            //Down left
-                            } else if buffer[down_left] == CellState::Dead && x > 0 {
-                                buffer[down_left] = CellState::Sand;
-                                buffer[pixel_pos] = CellState::Dead;
-
-                            //Down right
-                            } else if buffer[down_right] == CellState::Dead && x < width - 1 {
-                                buffer[down_right] = CellState::Sand;
-                                buffer[pixel_pos] = CellState::Dead;
-
-                            //Water collision
-                            } else if buffer[down] == CellState::Water {
-                                buffer[down] = CellState::Sand;
-                                buffer[pixel_pos] = CellState::Water;
-                            }
-                        }
-                    }
-
-                    CellState::Water => {
-                        //Down
-                        if buffer[down] == CellState::Dead && down < width * height {
-                            buffer[down] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-
-                        //Down left
-                        } else if buffer[down_left] == CellState::Dead && down < width * height {
-                            buffer[down_left] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-
-                        //Down right
-                        } else if buffer[down_right] == CellState::Dead && down < width * height {
-                            buffer[down_right] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-
-                        //Left
-                        } else if buffer[left] == CellState::Dead {
-                            info!("succes left");
-                            buffer[left] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-
-                        //Right
-                        } else if buffer[right] == CellState::Dead {
-                            info!("success right");
-                            buffer[right] = CellState::Water;
-                            buffer[pixel_pos] = CellState::Dead;
-                        }
-                    }
-                }
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
             }
         }
 
-        //Per-pixel coloring
-        for (i, _) in buffer.iter().enumerate() {
-            image.set_pixel(
-                (i % width) as u32,
-                (i / width) as u32,
-                match buffer[i] {
-                    CellState::Dead => BLACK,
-                    CellState::Water => BLUE,
-                    CellState::Sand => ORANGE,
-                },
-            );
-        }
-
-        texture.update(&image);
-
-        draw_texture(&texture, 0., 0., WHITE);
-
-        next_frame().await
+        canvas.clear();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 30));
+        // The rest of the game loop goes here...
     }
+
+    Ok(())
 }
